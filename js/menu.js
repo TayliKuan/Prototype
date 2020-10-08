62982e7eab857d1a9f49dda1bb2e211a5395f6e6
k9798909 <k9798909@gmail.com>
2020-10-08 13:53:51 +0800

    修改測試

--- js/menu.js
+++ js/menu.js
@@ -105,8 +105,13 @@ var menuHierarchy =[
             ['合格清冊補印','TBPR0W060L.html',],
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
             ['月試核異動清單補印','',],
             ['提繳時差轉入農退儲金月退專戶清單補印','',],
+=======
+            ['月試核異動清單補印','TBPR0W070L.html',],
+            ['提繳時差轉入農退儲金月退專戶清單補印','TBPR0W080L.html',],
+>>>>>>> Stashed changes
 =======
             ['月試核異動清單補印','TBPR0W070L.html',],
             ['提繳時差轉入農退儲金月退專戶清單補印','TBPR0W080L.html',],
